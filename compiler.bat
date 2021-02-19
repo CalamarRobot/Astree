24857a49a0e3f7fddcb2c4e2771348657f02f93c
CalamarRobot <xanbrion1606@gmail.com>
2021-02-19 02:24:46 +0100

    added icon to compiled version console

---
+++ compiler.bat
@@ -0,0 +1,2 @@
+pyinstaller --add-data "C:/Users/Utilisateur/AppData/Local/Programs/Python/Python39/Lib/site-packages/pyfiglet";./pyfiglet --onefile --icon=clmrconsole.ico Astrée.py && COPY /Y .\dist\Astrée.exe
+pause
